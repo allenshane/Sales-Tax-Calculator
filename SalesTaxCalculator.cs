9df37a8d04cbef90359bd78788a067a57217f59d
allenshane <48199028+allenshane@users.noreply.github.com>
2020-12-17 06:13:49 +0530

    Add files via upload
    Initial Commit

---
+++ SalesTaxCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SalesTaxCalculator
+{
+    public class SalesTaxCalculator
+    {
+        public static void Main()
+        {
+            string path = File.ReadAllLines(@"Settings.txt")[0];
+            var txtFiles = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string currentFile in txtFiles)
+            {
+                string[] lines = File.ReadAllLines(currentFile);
+                InputProcessor.ProcessInput(lines);
+                Console.WriteLine("--------------------------------------------------");
+            }
+            Console.ReadLine();
+        }
+    }
+}
